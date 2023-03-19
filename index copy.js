dae7298ba25400c280f7d8730c75381709fa3540
matt roche <matt.roche@gmail.com>
2023-03-19 17:20:20 -0500

    completed phase 1 arrow functions lab

---
+++ index copy.js
@@ -0,0 +1,12 @@
+function divide(a, b) {
+    return ((a)/(b));
+}
+let x = 2
+
+const square = x => x * x;
+
+let a = 3;
+let b = 4;
+
+const add = (a, b) => a + b;
+
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-function divide(a, b) {
-    return ((a)/(b));
+const divide = function() {
+    let a = 2000;
+    let b = 100;
+    return a / b;
 }
+
+const square = (x) => x * x;
+
 let x = 2
 
-const square = x => x * x;
+square(x);
+
+const add = (a, b) => a + b;
 
 let a = 3;
 let b = 4;
 
-const add = (a, b) => a + b;
-
+add(a, b);
